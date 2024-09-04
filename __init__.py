1708283e6f8233192b2edd817b038ffd89286b04
BIOKORO OGHENETEGA D. <110293491+Leommm-byte@users.noreply.github.com>
2024-09-04 18:22:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
+# Import main components
+from .config_util import *
+from .debug_util import *
+from .flush import *
+from .lora import *
+from .model_util import *
+from .prompt_util import *
+from .train_lora import *
+from .train_lora_xl import *
+from .train_util import *
 
+# You can also import specific functions if you prefer
+# from .config_util import some_specific_function
+# from .lora import another_specific_function
+
+# If you have a version number for your package, you can include it here
+__version__ = "0.1.0"
+
+# You can also include a short description of your package
+__description__ = "LECO: A package for [brief description of what your package does]"
